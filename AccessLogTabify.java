0e378d683e694bb375d28fe8b72641d9a4e93aaf
Mary Solbrig <secondemailforspam@gmail.com>
2015-03-20 05:19:44 -0700

    Added File Chooser
    Added a file chooser interface to the code

--- AccessLogTabify.java
+++ AccessLogTabify.java
@@ -1,6 +1,8 @@
 package AccessLogTabify;
 import java.io.IOException;
 
+import javax.swing.JFileChooser;
+
 //Example for calling ApacheLogToCSV on specific file.
 //This really should call something to have you navigate to the directory and file,
 //and then ask you what you would like to call the output,
@@ -9,13 +11,26 @@ import java.io.IOException;
 public class AccessLogTabify{
     
     public static void main(String[] args) throws IOException{
-        String masterDirectory = "C:/Users/msolbrig/workspace/AccessLogTabify/Example Log Files/Worker X/TableauServerlogs";
-        String outputFileName = "FormattedLogs.txt";
+    	
+    	JFileChooser fileChooser = new JFileChooser();
+    	fileChooser.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY);
+    	
+    	if(fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+    		System.out.println("good job choosing a file");
+    		System.out.println("The file you chose was: " + fileChooser.getSelectedFile().toString());
+    		
+    		String masterDirectory = fileChooser.getSelectedFile().toString();
+            String outputFileName = "FormattedLogs.txt";
 
+    		ApacheLogToCSV logConverter = new ApacheLogToCSV(masterDirectory,outputFileName);
+            
+            logConverter.combineLogs();
+
+    	}
+    	else{
+    		System.err.println("no file was selected");
+    	}    
         
-        ApacheLogToCSV thing = new ApacheLogToCSV(masterDirectory,outputFileName);
-        
-        thing.combineLogs();
     }
 
 }
